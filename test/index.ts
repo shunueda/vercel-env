cf2238deb93b01a4bb0a784112cfca5d3de1e5ad
Shun Ueda <62182668+shunueda@users.noreply.github.com>
2024-05-29 14:08:54 -0700

    add tests

--- test/index.ts
+++ test/index.ts
@@ -1,1 +1,11 @@
-console.log(process.env)
+import { getInput, setFailed } from '@actions/core'
+
+const expectedKey = getInput('key')
+const expectedValue = getInput('value')
+
+const value = process.env[expectedKey]
+
+if (value !== expectedValue) {
+  setFailed(`Expected ${expectedKey} to be ${expectedValue}, but got ${value}`)
+}
+console.log(`✅ ${expectedKey} is ${expectedValue}`)
